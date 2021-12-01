5ff44832a751355b029567f8d9a0f7648d9e501b
Celia Macas <celiamacas24@gmail.com>
2021-12-01 13:53:26 -0500

    Crear componentes

--- HolaMundo/src/app/app.module.ts
+++ HolaMundo/src/app/app.module.ts
@@ -4,17 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent} from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { TablaComponent } from './dashboard/tabla/tabla.component';
+import { ListaComponent } from './dashboard/lista/lista.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    FooterComponent,
+    TablaComponent,
+    ListaComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
---
+++ HolaMundo/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,17 @@
+import { Template } from "@angular/compiler/src/render3/r3_ast";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-dashboard',
+    template: '<h1> Hola Mundo </h1>',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
+
+
+
+})
+
+
+export class DashboardComponent {
+
+}
